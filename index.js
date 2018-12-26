42f16d7a5dee9fd12a7b4d698436eb2dfaceaba9
sensible-shell-8529 <diana.lee116@gmail.com>
2018-12-26 19:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 function dwarfRollCall(dwarves) {
+  var call =`1.'+ dwarves[0]`
+  for(var i=1; i<dwarves.length; i++){
+    call += `${i+1}. ${dwarves[i]}`
+  }return call.tostring()
 }
 
 function summonCaptainPlanet(planeteerCalls){
