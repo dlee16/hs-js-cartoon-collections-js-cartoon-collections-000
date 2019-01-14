a6d7cdcb9ea0bc6c4c3064ff058d7acaa8369e6b
dlee16 <45607685+dlee16@users.noreply.github.com>
2019-01-14 14:57:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
-  var call =`1. ${dwarves[0]} `;
-  for(var i=1; i<dwarves.length; i++){
+  var call =`1. ${dwarves[dwarves.length/2]} `;
+  for(var i=dwarves.length/2 + 1; i<dwarves.length; i++){
     call += `${i+1}. ${dwarves[i]} `;
   }return String(call);
 }
@@ -21,10 +21,30 @@ function longPlaneteerCalls(words) {
 }
 
 function findTheCheese (foods) {
-  var cheese =["cheddar", "gouda", "camembert"];
+  var cheese =["cheddar", "gouda", "camembert", "swiss"];
   for (var i = 0; i<foods.length; i++){
-    if (foods[i]=== cheese[0] || foods[i]=== cheese[1] || foods[i]=== cheese[2]){
+    if (foods[i]=== cheese[0] || foods[i]=== cheese[1] || foods[i]=== cheese[2] || foods[i]===cheese[3]){
       return foods[i];
     }
   }return "no cheese!";
 }
+
+// ['banana', 'cheddar', 'apple'] -> "cheddar"
+// ["berry", "cheddar", "apple", "gouda"] -> "cheddar"
+
+function wordsThatStartWithB(words){
+  var bWords=[]; 
+  for (var i =0; i<words.length; i++){
+    if (words[i].startWith("b")){
+      bWords.push(words[i])
+    }
+  }
+  return bWords
+}
+        
+ // "brian".startsWith("b") => true
+        // "emily".startsWith("b") => false
+
+wordsThatStartWithB(['brian', 'joe', 'alex']) // ['brian']
+wordsThatStartWithB(['berry', 'banana', 'cherry']) // ['berry', 'banana']
+wordsThatStartWithB(['cher', 'chad', 'chad2']) // []
